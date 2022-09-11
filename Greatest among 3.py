c43b576ae243ab3e89fe91f5844e0400f97d5f28
20CSESumanth <112487116+20CSE452-Sumanth@users.noreply.github.com>
2022-09-11 22:39:33 +0530

    Add files via upload

---
+++ Greatest among 3.py
@@ -0,0 +1,9 @@
+a=float(input("enter 1st"))
+b=float(input("enter 2nd"))
+c=float(input("Enter 3rd"))
+if(a>b and a>c):
+    print(a,"is greatest")
+elif(b>c and b>a):
+    print(b,"is greatest")
+else:
+    print(c,"is greatest")
